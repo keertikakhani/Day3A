a973ded5b1842bea4b236cc59136fb8bf3f015ac
Your Name <you@example.com>
2023-07-17 22:05:48 +0530

    program for class and object concept

--- Day3A/Program.cs
+++ Day3A/Program.cs
@@ -6,6 +6,9 @@ namespace Day3A
     {
         public static void Main(string[] args)
         {
+            Car car = new Car();
+            Console.WriteLine(car.color);
+
             Console.ReadLine();
         }
     }
